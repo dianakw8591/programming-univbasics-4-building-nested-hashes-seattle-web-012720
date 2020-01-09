c8d5b1461d97e7cc768394cac75ca663895292af
Diana Wendt <dianakw@gmail.com>
2020-01-09 20:20:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
   {railroads: {
   	  pieces: 4,
   	  rent_in_dollars: {
-  	    1:,
-  	    2:,
-  	    3:,
-  	    4:
+  	    one_piece_owned: 25,
+  	    two_pieces_owned: 50,
+  	    three_pieces_owned: 75,
+  	    four_pieces_owned: 100
   	  }
   	}
   	}
